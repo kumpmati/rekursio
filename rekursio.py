96802063fa20fe0dec80f634c7547b29aecb28b0
kumpmati <kumpulainen.matias@gmail.com>
2019-09-24 14:58:29 +0300

    saatanan tunari
    ei se taso 2 toiminu täysin

--- rekursio.py
+++ rekursio.py
@@ -1,4 +1,2 @@
 #kuinka syvälle pääset?
 exec("\n".join(input().split(";")))
-#taso 2:
- #ohjelma = [];me=input();while me!="/*suorita":;     me=input();     ohjelma.append(me);exec("\n".join(ohjelma))
