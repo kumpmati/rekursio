9e63734e5cba34dd78833158895b54588044aa6f
kumpmati <kumpulainen.matias@gmail.com>
2019-09-24 14:34:27 +0300

    lisättiin rekursion taso
    kommentissa on koodi jolla voidaan suorittaa multiline python koodia tämän sisällä

--- rekursio.py
+++ rekursio.py
@@ -1,2 +1,4 @@
 #kuinka syvälle pääset?
 exec("\n".join(input().split(";")))
+#taso 2:
+  #ohjelma = [];me=input();while me!="/*suorita":; me=input();     ohjelma.append(me);     print(me);exec("\n".join(ohjelma))
